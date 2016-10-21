d39821eba9cb806210ce28138a3de4ed91469da8
Heejong Ahn <heejongahn@gmail.com>
2016-10-22 01:25:35 +0900

    Add basic react structure

--- app/main.js
+++ app/main.js
@@ -1,1 +1,9 @@
-console.log(1);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './components';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('main')
+);
