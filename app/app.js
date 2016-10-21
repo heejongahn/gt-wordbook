151c8120e3665f3f554e14c86ccdf9cf5af4903d
Heejong Ahn <heejongahn@gmail.com>
2016-10-21 23:51:33 +0900

    Add index.html serving

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import Koa from 'koa';
+import send from 'koa-send';
+import serve from 'koa-static';
+import logger from 'koa-logger';
+
+const app = Koa();
+
+app.use(logger());
+app.use(function* index() {
+  yield send(this, './index.html');
+})
+
+app.listen(3000);
