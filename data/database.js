bad4ea415f7779306223bc48d644027b85372128
Heejong Ahn <heejongahn@gmail.com>
2016-10-23 17:37:51 +0900

    Add sqlite db auth logic

--- data/database.js
+++ data/database.js
@@ -1,6 +1,22 @@
 import Sequelize from 'sequelize';
 
-const db = new Sequelize(`sqlite:///${__dirname}/db.sqlite`, {});
+const db = new Sequelize('db', null, null, {
+  host: 'localhost',
+  dialect: 'sqlite',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  storage: `${__dirname}/db.sqlite`
+});
+
+db.authenticate()
+  .then((err) => {
+    console.log('Connection has been established successfully.');
+  }, (err) => {
+    console.log('Unable to connect to the database:', err);
+  });
 
 const User = db.define('user', {
   username: {
