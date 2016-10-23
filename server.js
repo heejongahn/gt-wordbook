a03f955963017d53b42f017a9b79fb2f06a753f2
Heejong Ahn <heejongahn@gmail.com>
2016-10-23 16:04:16 +0900

    Add basic hello world graphql example

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ import Koa from 'koa';
 import send from 'koa-send';
 import serve from 'koa-static';
 import logger from 'koa-logger';
+import mount from 'koa-mount';
+import graphqlHTTP from 'koa-graphql';
+
+import schema from './data/schema';
 
 const app = Koa();
 
 app.use(logger());
 app.use(serve(`${__dirname}/public`));
+
+app.use(mount('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+})));
 
 app.use(function* index() {
   yield send(this, './index.html');
